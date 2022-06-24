f248e945dfd3010bc5bc73f0650afd1ff1f26b60
Abhinay Pratap <abhinaypratap@icloud.com>
2022-06-25 03:14:22 +0530

    Create Project

---
+++ Essentials/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Essentials
+//
+//  Created by Abhinay Pratap on 25/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
