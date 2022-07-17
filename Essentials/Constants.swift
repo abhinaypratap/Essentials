e42531330c31a3a277b21e7a6164ae130906511d
Abhinay Pratap <abhinaypratap@icloud.com>
2022-07-17 16:16:44 +0530

    Create constants

--- Essentials/Constants.swift
+++ Essentials/Constants.swift
@@ -5,4 +5,13 @@
 //  Created by Abhinay Pratap on 25/06/22.
 //
 
-import Foundation
+import UIKit
+
+struct K {
+    // Segues
+    static let segue1Identifier = "1"
+    
+    // Cells
+    static let cell1Identifier = "2"
+    static let cell2Identifier = "3"
+}
